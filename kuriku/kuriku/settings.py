e10f854cbaeefc5d9079507f652221203b1ee9c0
iharjanto <iharjanto@gmail.com>
2025-07-20 03:49:28 +0000

    production change

--- kuriku/kuriku/settings.py
+++ kuriku/kuriku/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lnbaq992u10ye1riej&qqw++7ajq55mc#v0(ho33!hu9$0urzk
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['pymad34.pythonanywhere.com', 'localhost']
+ALLOWED_HOSTS = ['pymad34.pythonanywhere.com', 'localhost','elektroupgris.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
